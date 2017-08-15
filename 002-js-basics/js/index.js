01f64217c51cf4842c5d1c706ca5314519ada7c5
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-08-15 13:38:29 +0800

    If else statement with cohession type compare ('==' vs '===')

--- 002-js-basics/js/index.js
+++ 002-js-basics/js/index.js
@@ -1,4 +1,45 @@
 console.log('Hello World outside HTML');
 
-var name = 'Prajesh';
-console.log(name);
+/*
+// Variables
+var first_name = 'Prajesh';
+var last_name = 'Ananthan';
+
+console.log(first_name + last_name);
+
+var age = 30;
+console.log(age);
+
+var isMarried = true;
+console.log(isMarried);
+
+if (isMarried) {
+    console.log(first_name + ' '  + last_name + " is married!")
+}
+
+// Variable mutation
+// Meaning the data type of the variable has changed
+age = 'thirty';
+console.log(age);
+
+alert(age);
+*/
+
+
+var birthDate = 2017;
+var age = 30;
+console.log(2017 - 30);
+
+// Conditional statement
+if (age == 30) {
+    console.log("You are now an adult!");
+}
+
+// Type cohession
+if (23 == "23") {
+    console.log("No type cohession!");
+}
+
+if (23 === "23") {
+    console.log("With type cohession");
+}
